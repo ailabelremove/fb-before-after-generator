e992df75eef6ed2dd8fc39be54a8abc7aa80f346
ailabelremove <ailabelremove@gmail.com>
2026-09-10 16:34:45 +0600

    Create defaultTemplate.ts with initial profile state
    Initialize default template with profile data and metrics.

---
+++ lib/defaultTemplate.ts
@@ -0,0 +1,19 @@
+import { TemplateState } from "./types";
+
+export const defaultTemplate: TemplateState = {
+  profileName: "Dylon",
+  time: "2h",
+  caption: "Beautiful charm in my village home! #AlorNot",
+  mainImage: null,
+  profileImage: null,
+  left: {
+    likes: "150",
+    comments: "12",
+    shares: "3",
+  },
+  right: {
+    likes: "4.5K",
+    comments: "320",
+    shares: "95",
+  },
+};
