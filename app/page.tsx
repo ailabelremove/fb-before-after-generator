71a2f3a01beee8512e50a5155b63ceecd7e1fd0c
ailabelremove <ailabelremove@gmail.com>
2026-09-10 20:00:15 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,10 @@ function AppBody() {
         </div>
 
         {/* Preview */}
-        <div className="overflow-auto rounded-xl bg-white p-2 shadow-sm ring-1 ring-gray-200">
-          <TemplatePreview ref={previewRef} template={template} />
+        <div className="overflow-x-auto rounded-xl bg-white shadow-sm ring-1 ring-gray-200">
+          <div className="min-w-[640px]">
+            <TemplatePreview ref={previewRef} template={template} />
+          </div>
         </div>
       </div>
     </div>
